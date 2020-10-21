0012abbece804d41b34352b3a4fb224bbdbad431
cphsiung <chunpeihs@gmail.com>
2020-10-20 20:27:31 -0400

    Update area.java

--- area.java
+++ area.java
@@ -9,11 +9,12 @@ import java.util.Scanner;
 
 public class area
 {
-   public static int validator (int n) {
+   // method for validating once if entry is greater than 0
+   public static double validator (double n) {
       Scanner Input = new Scanner(System.in);
       if (n <= 0) {
          System.out.println("Please enter a number greater than 0!");
-         n = Input.nextInt();
+         n = Input.nextDouble();
        }
       return n;
    }
@@ -22,51 +23,52 @@ public class area
       
       {
          // Declaration
-         int choice, userentry, rectangle, length, width, triangle, base, height, circle, radius, square, side;
-         boolean startloop = true;
+         int choice; 
+         double userentry, rectangle = 0, length = 0, width = 0, triangle = 0, base = 0, height = 0, circle = 0, radius = 0, square = 0, side = 0;
          Scanner Input = new Scanner(System.in);      
          
          // Input
-         while(startloop) {
-            System.out.println("Welcome to shape area calculator! Please choose shape you want to calculate:\n\tRectangle - enter 1\n\tTriangle - enter 2\n\tCircle - enter 3\n\tSquare - enter 4");
-            choice = Input.nextInt();
-   
-            switch (choice) {
-               case 1:  System.out.println("Rectangle! Please enter length:");
-                        userentry = Input.nextInt();
-                        userentry = validator(userentry);
-                        length = userentry;
-                        System.out.println("Please enter width:");
-                        userentry = Input.nextInt();
-                        userentry = validator(userentry);
-                        width = userentry;
-                        startloop = false;
-                        break;
-               case 2:  System.out.println("Triangle! Please enter base:");
-                        userentry = Input.nextInt();
-                        userentry = validator(userentry);
-                        base = userentry;
-                        System.out.println("Please enter height:");
-                        userentry = Input.nextInt();
-                        userentry = validator(userentry);
-                        height = userentry;         
-                        startloop = false;
-                        break;
-               case 3:  System.out.println("Circle! Please enter radius:");
-                        userentry = Input.nextInt();
-                        userentry = validator(userentry);
-                        radius = userentry;
-                        startloop = false;
-                        break;
-               case 4:  System.out.println("Square! Please enter side:");
-                        userentry = Input.nextInt();
-                        userentry = validator(userentry);
-                        side = userentry;
-                        startloop = false;
-                        break;
-               default: System.out.println("Invalid choice, please try again.");
+         System.out.println("Welcome to shape area calculator! Please choose shape you want to calculate:\n\tRectangle - enter 1\n\tTriangle - enter 2\n\tCircle - enter 3\n\tSquare - enter 4");
+         choice = Input.nextInt();
+
+         switch (choice) {
+            case 1:  // Rectangle
+                     System.out.println("Rectangle! Please enter length:");
+                     userentry = Input.nextDouble();
+                     userentry = validator(userentry); // validate if entry greater than 0
+                     length = userentry; // store user entry to length
+                     System.out.println("Please enter width:");
+                     userentry = Input.nextDouble();
+                     userentry = validator(userentry);
+                     width = userentry;
+                     break;
+            case 2:  // Triangle
+                     System.out.println("Triangle! Please enter base:");
+                     userentry = Input.nextDouble();
+                     userentry = validator(userentry);
+                     base = userentry;
+                     System.out.println("Please enter height:");
+                     userentry = Input.nextDouble();
+                     userentry = validator(userentry);
+                     height = userentry;         
+                     break;
+            case 3:  // Circle
+                     System.out.println("Circle! Please enter radius:");
+                     userentry = Input.nextDouble();
+                     userentry = validator(userentry);
+                     radius = userentry;
+                     break;
+            case 4:  // Square
+                     System.out.println("Square! Please enter side:");
+                     userentry = Input.nextDouble();
+                     userentry = validator(userentry);
+                     side = userentry;
+                     break;
+            default: // all other choices
+                     System.out.println("Invalid choice, please try again.");
+                     break;
             }
-         }
+            
          // Process
          rectangle = length * width;
          triangle = (base * height)/2;
@@ -76,9 +78,17 @@ public class area
          // Output
          switch (choice) {
             case 1:
+               System.out.println("Rectangle\n length = " + length + " width = " + width + "\n area = " + rectangle);
+               break;
             case 2:
+               System.out.println("Triangle\n base = " + base + " height = " + height + "\n area = " + triangle);
+               break;
             case 3:
+               System.out.println("Circle\n radius = " + radius + "\n area = " + circle);
+               break;
             case 4:
+               System.out.println("Square\n side = " + side + "\n area = " + square);
+               break;
          }
       }
 

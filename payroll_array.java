6580f88333521370060e5c6ef46050ef700c3cc2
cphsiung <chunpeihs@gmail.com>
2020-11-17 16:33:05 -0500

    Update payroll_array.java

--- payroll_array.java
+++ payroll_array.java
@@ -37,12 +37,14 @@ public class payroll_array
             middleInit[i] = Input.next().charAt(0);
             System.out.println("Please enter last name for employee" + (i + 1) + " :");
             lastName[i] = Input.next();
-            System.out.println("Please enter hours worked for employee" + (i + 1) + " :");
-            hours[i] = Input.nextDouble();
+            System.out.println("Please enter employee" + (i + 1) + " pay rates per hour:");
+            rates[i] = Input.nextDouble();
             while (rates[i] < 15 || rates[i] > 50) {
                System.out.println("Rates must be between 15 to 50.");
                rates[i] = Input.nextDouble();
             } // while loop for data validation
+            System.out.println("Please enter hours worked for employee" + (i + 1) + " :");
+            hours[i] = Input.nextDouble();
             while (hours[i] < 0 || hours[i] > 60) {
                System.out.println("Hours must be between 0 to 60.");
                hours[i] = Input.nextDouble();
@@ -54,12 +56,12 @@ public class payroll_array
          for (int k = 0; k < size; k++) {
             // Calculate additional overtime pay
             if (hours[k] > 40) {
-               overtime[k] = (hours[k] - 40) * rate[k] * overtimeRate;
+               overtime[k] = (hours[k] - 40) * rates[k] * overtimeRate;
             } else {
                overtime[k] = 0;
             }
             // Calculate gross pay
-            grossPay[k] = rates[k] * hours[k] + overtime[k];
+            grossPay[k] = hours[k] * rates[k] + overtime[k];
             // Calculate State Tax amount
             stateTax[k] = grossPay[k] * stateTaxRate;
             // Calculate Federal Tax amount
@@ -78,27 +80,27 @@ public class payroll_array
          avg = total/size;
          
          // OUTPUT
-         System.out.println("                    Data Housing Corp. Weekly Payroll");
-         System.out.println("                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-         System.out.printf("%-12s %-3s %-12s %8s %8s %8s %8s %8s %8s %8s\n", 
+         System.out.println("                         Data Housing Corp. Weekly Payroll");
+         System.out.println("                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+         System.out.printf("%-12s %-3s %-11s %-10s %-8s %-10s %-10s %-10s %-12s %-10s\n", 
                            "First Name", "MI", "Last Name", "Rate/Hour", "Hours", "Gross", "State Tax", "Fed Tax", "Union Fees", "Net");
 
          for (int j = 0; j < size; j++) {
             System.out.printf("%-13s", firstName[j]);
             System.out.printf("%-4C", middleInit[j]); // capital C to format to capital letter
-            System.out.printf("%-12s", lastName[j]);
-            System.out.printf("%$9.2f", rates[j]);
-            System.out.printf("%9.0f", hours[j]);
-            System.out.printf("%$9.2f", Gross[j]);
-            System.out.printf("%$9.2f", stateTax[j]);
-            System.out.printf("%$9.2f", fedTax[j]);
-            System.out.printf("%$9.2f", unionFee[j]);
-            System.out.printf("%$9.2f\n", netPay[j]);
+            System.out.printf("%-13s", lastName[j]);
+            System.out.printf("$%-10.2f", rates[j]);
+            System.out.printf("%-8.0f", hours[j]);
+            System.out.printf("$%-10.2f", grossPay[j]);
+            System.out.printf("$%-10.2f", stateTax[j]);
+            System.out.printf("$%-10.2f", fedTax[j]);
+            System.out.printf("$%-12.2f", unionFee[j]);
+            System.out.printf("$%-10.2f\n", netPay[j]);
          }
          
          System.out.print("\nGross Pay for all employees\t: ");
-         System.out.printf("%$.2f", total);
-         System.out.print("\nGross Pay for all employees\t: ");
-         System.out.printf("%$.2f", avg);
+         System.out.printf("$%.2f", total);
+         System.out.print("\nAverage Pay for all employees\t: ");
+         System.out.printf("$%.2f", avg);
       }
 }

8b7bb3cb99c72bd6f2d9350a0fda3b9f8c0eebc9
cphsiung <chunpeihs@gmail.com>
2020-11-30 22:11:05 -0500

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,31 @@
+// Chunpei Hsiung
+// due date: 12/2 8:00am
+// Prof. Aljamal
+// Java Programming CSC-117-01
+/* program description
+   Using methods to write a java program. */
+
+import java.util.Scanner;
+public class methods
+{
+   public static void main(String[] args) {
+      // DECLARATION
+      double First, Second, Total;
+      // INPUT      
+      // PROCESS
+      // OUTPUT
+   }
+   
+   public static double Input() {
+      double num;
+      Scanner Input = new Scanner(System.in);
+      System.out.println("Please enter a number:");
+      num = Input.nextDouble();
+      return num;
+   }
+   
+   public static void Greetings() {
+      System.out.println("Welcome to the Java program!");
+   }
+
+}
